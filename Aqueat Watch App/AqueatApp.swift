c2335e1c74b22fdb241ba3f9f4718f1a516ffcd6
Leo Dion <leogdion@brightdigit.com>
2023-04-19 19:43:20 -0400

    Initial Commit

---
+++ Aqueat Watch App/AqueatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AqueatApp.swift
+//  Aqueat Watch App
+//
+//  Created by Leo Dion on 4/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Aqueat_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aqueat/AqueatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AqueatApp.swift
+//  Aqueat
+//
+//  Created by Leo Dion on 4/19/23.
+//
+
+import SwiftUI
+
+@main
+struct AqueatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
